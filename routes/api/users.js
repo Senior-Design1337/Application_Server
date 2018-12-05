d5ffc8cb9229249ed0d05f7ae8ea524b15a55174
zerkclown <zerkclown@gmail.com>
2018-12-05 15:29:21 +0400

    fix

--- routes/api/users.js
+++ routes/api/users.js
@@ -166,7 +166,7 @@ ref.on("value", function(snapshot) {
               // score: '850',
               // time: '2:45'
               // "user": user.toString(),
-              'id': "5be97090298f43633d6a3fed",
+              // 'id': "5be97090298f43633d6a3fed",
               'id': "5bd910c1dc97ff7902ff8cfd",
               'email': "asdasd",
               'name': "user.name",
